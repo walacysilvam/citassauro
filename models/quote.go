dbec0498887b378425123aafbb0902e751725647
Walacy Silva <walacysm@gmail.com>
2025-07-02 21:32:32 -0300

    adicionado binding ao modelo de quote

--- models/quote.go
+++ models/quote.go
@@ -1,10 +1,14 @@
 package models
 
+import (
+	"time"
+)
+
 type Quote struct {
-	ID uint  `json:"id" gorm:"primaryKey"`
-	Author string `json:"author"`
-	Text string `json:"text"`
-	Votes int `json:"votes"`
-	CreatedAt string `json:"created_at"`
-	UpdatedAt string `json:"updated_at"`
+	ID        uint      `json:"id" gorm:"primaryKey"`
+	Author    string    `json:"author" bind:"required"`
+	Text      string    `json:"text" bind:"required"`
+	Votes     int       `json:"votes"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
